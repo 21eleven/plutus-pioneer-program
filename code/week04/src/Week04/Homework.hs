5936c8df85fe618bcf9c1824d77e13000c99a219
Noah Lidell <noahlidell@gmail.com>
2022-09-04 17:34:58 -0500

    w4 w custom types

--- code/week04/src/Week04/Homework.hs
+++ code/week04/src/Week04/Homework.hs
@@ -30,9 +30,26 @@ type PaySchema = Endpoint "pay" PayParams
 ada :: Integer -> Integer
 ada amt = amt * 1_000_000
 
+-- newtype ADA = ADA
+--     { val :: Integer
+--     } deriving (Show, Generic)
+--
+newtype ADA = ADA Integer
+newtype LL = LL Integer
+
+class AsLovelace a where
+    toLovelace :: a -> Integer
+
+instance AsLovelace ADA where
+    toLovelace (ADA x) = x * 1_000_000
+
+instance AsLovelace LL where
+    toLovelace (LL x) = x
+
 payContract :: Contract () PaySchema Text ()
 payContract = do
     pp <- awaitPromise $ endpoint @"pay" return
+
     let tx = mustPayToPubKey (ppRecipient pp) $ lovelaceValueOf $ ppLovelace pp
     handleError (\err -> logError $ "Hit Error: " ++ unpack err) $ void $ submitTx tx
     payContract
@@ -40,24 +57,26 @@ payContract = do
 -- A trace that invokes the pay endpoint of payContract on Wallet 1 twice, each time with Wallet 2 as
 -- recipient, but with amounts given by the two arguments. There should be a delay of one slot
 -- after each endpoint call.
-payTrace :: Integer -> Integer -> EmulatorTrace ()
+payTrace :: (AsLovelace currency) => currency -> currency -> EmulatorTrace ()
 payTrace amt1 amt2 = do
     h <- activateContractWallet (knownWallet 1) payContract
     let pkh = mockWalletPaymentPubKeyHash $ knownWallet 2
     callEndpoint @"pay" h PayParams
         { ppRecipient=pkh
-        , ppLovelace=amt1
+        , ppLovelace=toLovelace amt1
         }
     void $ Emulator.waitNSlots 1
     callEndpoint @"pay" h PayParams
         { ppRecipient=pkh
-        , ppLovelace=amt2
+        , ppLovelace=toLovelace amt2
         }
     void $ Emulator.waitNSlots 1
 
 payTest1 :: IO ()
-payTest1 = runEmulatorTraceIO $ payTrace (ada 10) (ada 20)
+payTest1 = runEmulatorTraceIO $ payTrace (ADA 10) (ADA 20)
 
 payTest2 :: IO ()
-payTest2 = runEmulatorTraceIO $ payTrace (ada 1000) (ada 20)
+payTest2 = runEmulatorTraceIO $ payTrace (ADA 1000) (ADA 20)
 
+payTest3 :: IO ()
+payTest3 = runEmulatorTraceIO $ payTrace (LL 1_000_000) (LL 2_000_000)
